f5c09e40d1fcba9577a37352034f79f1d708db11
Maxx Wilson - Laptop <JesseMaxxWilson@utexas.edu>
2020-11-20 21:42:24 -0600

    Added Live Feed Hough Circles

--- LiveFeed.py
+++ LiveFeed.py
@@ -58,6 +58,24 @@ try:
 
         #cv2.imshow('RealSense', cv2.Canny(color_image, 200, 250))  # 400/550
 
+        # detect circles in the image
+        circles = cv2.HoughCircles(thresh1, cv2.HOUGH_GRADIENT, 1, 36, param1=100, param2=7, minRadius = 20, maxRadius = 25)
+
+
+        # ensure at least some circles were found
+        if circles is not None:
+            print("active")
+            # convert the (x, y) coordinates and radius of the circles to integers
+            circles = np.round(circles[0, :]).astype("int")
+            # loop over the (x, y) coordinates and radius of the circles
+            for (x, y, r) in circles:
+                # draw the circle in the output image, then draw a rectangle
+                # corresponding to the center of the circle
+                cv2.circle(diff, (x, y), r, (0, 0, 255), 4)
+                cv2.rectangle(diff, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
+            # show the output image
+            cv2.imshow("output", diff)
+
         cv2.waitKey(1)
 
 finally:
